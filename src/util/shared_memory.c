6a7858246d9ba57e6df970c4c28cfca2a7a9fcc5
Francesco Mauro <francesco.mauro@educ.di.unito.it>
2024-03-30 10:48:11 +0100

    solved shmget error
    Signed-off-by: Francesco Mauro <francesco.mauro@educ.di.unito.it>

--- src/util/shared_memory.c
+++ src/util/shared_memory.c
@@ -1,26 +1,26 @@
 
 #include "shared_memory.h"
 #include "hash_table.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <errno.h>
 
+#define KEY 0x200
 
 struct hash_table *attach_shared_memory(){
-  key_t key = ftok("./bin/master",65);
-  if(key == -1){
-    fprintf(stderr,"Erro in ftok shared memory");
-    exit(EXIT_FAILURE);
-  }
-  int shmid = shmget(key,sizeof(struct hash_table),0666);
+  
+  int shmid = shmget(KEY,sizeof(struct hash_table *),IPC_CREAT|0666);
   if(shmid == -1){
-    fprintf(stderr,"Erro in shmget shared memory");
+    fprintf(stderr,"Error in shmget shared memory %s\n",strerror(errno));
     exit(EXIT_FAILURE);
   }
 
   struct hash_table *stats_map = (struct hash_table *)shmat(shmid, NULL,0);
   if(stats_map == (void *) -1){
-    fprintf(stderr,"Error attaching in shared memory");
+    fprintf(stderr,"Error attaching in shared memory\n");
     exit(EXIT_FAILURE);
   }
 
@@ -30,7 +30,7 @@ struct hash_table *attach_shared_memory(){
 
 void detach_shared_memory(struct hash_table *stats_map){
   if(shmdt(stats_map) == -1){
-    fprintf(stderr,"Error detaching in shared memory");
+    fprintf(stderr,"Error detaching in shared memory\n");
     exit(EXIT_FAILURE);
   }
 }
